c72cfedcf96370134411c76bbc12110c662296b6
sukriti-79 <kamanisukriti19@gmail.com>
2026-09-12 04:13:07 +0530

    Complete Session 1 string assignments

---
+++ src/main/java/string/assigment_problems/ExamHallSeatDuplicationChecker.java
@@ -0,0 +1,43 @@
+package string.assigment_problems;
+
+public class ExamHallSeatDuplicationChecker {
+
+    public static void checkDuplicateSeats(int[] seatNumbers) {
+
+        boolean duplicateFound = false;
+
+        for (int i = 0; i < seatNumbers.length; i++) {
+
+            for (int j = i + 1; j < seatNumbers.length; j++) {
+
+                if (seatNumbers[i] == seatNumbers[j]) {
+
+                    boolean alreadyPrinted = false;
+
+                    for (int k = 0; k < i; k++) {
+                        if (seatNumbers[k] == seatNumbers[i]) {
+                            alreadyPrinted = true;
+                            break;
+                        }
+                    }
+
+                    if (!alreadyPrinted) {
+                        System.out.println("Duplicate Seat Number Found: " + seatNumbers[i]);
+                        duplicateFound = true;
+                    }
+                }
+            }
+        }
+
+        if (!duplicateFound) {
+            System.out.println("No Duplicate Seats Found");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] seatNumbers = {101, 102, 103, 102, 105};
+
+        checkDuplicateSeats(seatNumbers);
+    }
+}
